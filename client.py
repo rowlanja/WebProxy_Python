6fdeb2ca92319e95f60922e6b1d79692ae6321fc
rowlanja <rowlanja@tcd.ie>
2021-03-11 18:39:49 +0000

    blacklist and time and memory done

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
-import requests as req
+import requests, json
 from threading import Thread
 import os, time, ssl
 
 
 #resp = req.get("http://localhost:8083/http://www.example.com/")
-resp = req.get("http://localhost:8081/https://www.example.com/")
-print(resp.text)
+#resp = req.get("http://localhost:8081/https://www.example.com/")
+#print(resp.text)
 
+url = 'http://localhost:8084'
+payload = {
+            'user'  : 'manager',
+            'pswrd' : 'manager',
+            'func'  : 'blklst',
+            'url'   : 'www.example.com'
+}
+headers = {'content-type': 'application/json'}
 
+r = requests.post(url, data=json.dumps(payload), headers=headers)
 # resp = req.get("http://localhost:8081/usr=mngr&pswrd=mngr")
-# print(resp.text)
+print(resp.text)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket, sys, os, time
+import socket, sys, os, time, json, time
 from cachetools import Cache, TTLCache
 from _thread import *
 
@@ -20,7 +20,8 @@ except KeyboardInterrupt:
 
 max_conn = 5 #Maximum connections queues
 buffer_size = 16000 #Maximum socket's buffer size
-cache = TTLItemCache(maxsize=30,ttl=99000)
+cache = TTLItemCache(maxsize=30,ttl=99000) #Initializing my TTL cache
+blacklist = []  # Intializing my blacklisted URLS
 
 def start():    #Main Program
     try:
@@ -29,23 +30,28 @@ def start():    #Main Program
         s.listen(max_conn) #Start listening for connections
     except Exception: #Will be executed if anything fails
         sys.exit(2)
-    threadID = 0
 
+    threadID = 0
     while 1:
         conn, addr = s.accept() #Accept connection from client browser
         try:
             data = conn.recv(buffer_size) #Recieve client data
-            req = data.decode().split('\n')[0]
-            if not "favicon" in req :
-                start_new_thread(conn_string, (conn,data, addr, threadID)) #Starting a thread
+            decodeData = data.decode().split('\n')
+            req = decodeData[0]
+            if '"user": "manager", "pswrd": "manager"' in data.decode() : #IF WE RECIEVE A PROXY MANAGEMENT REQUEST
+                start_new_thread(handleManagerReq, (decodeData,threadID))
                 threadID += 1
+            else :
+                if not "favicon" in req :   #IF WE RECIEVE A NORMAL REQUEST
+                    start_new_thread(conn_string, (conn,data, addr, threadID, time.time()))
+                    threadID += 1
         except KeyboardInterrupt:
             s.close()
             print("\n[*] Proxy server shutting down....")
             print("[*] Have a nice day... ")
             sys.exit(1)
 
-def conn_string(conn, data, addr, threadID):
+def conn_string(conn, data, addr, threadID, startTime):
     first_line = data.decode().split('\n')[0]
     url = first_line
     http_pos = url.find("://")
@@ -68,31 +74,29 @@ def conn_string(conn, data, addr, threadID):
         port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
         webserver = temp[:port_pos]
     webserver =  webserver.strip('HTTP')
-
     get = str('GET '+temp.replace(webserver,'')+'\n')
     host = 'Host:'+webserver+'\n\n'
     dataPackage = ((get+host).encode())
-    print("[*] threadID : ", threadID)
-    # print(
-    #      "\n data : ", data,
-    #      "\n addr : ", addr,
-    #      "\n fl : ", first_line,
-    #      "\n temp : ", temp,
-    #      "\n port pos : " , port,
-    #      "\n webserver : " , webserver,
-    #      "\n decoded : ", first_line.encode(),
-    #      "\n GET text : ", get,
-    #      "\n Combined : ", dataPackage,
-    #      "\n HOST text : ", host
-    # )
-    proxy_server(webserver, port, conn, first_line, dataPackage, temp)
+    fullURL = temp.replace('/ HTTP/1.1', '').strip()  # remove HTTP version from URL
+    if fullURL not in blacklist:
+        proxy_server(webserver, port, conn, first_line, dataPackage, fullURL, threadID, startTime)
+    else :
+        print("[*] Request rejected, blacklisted URL")
+        while True:
+            rawString = "HTTP/1.1 200 OK\n"+"Content-Type: text/html\n"+"\n"+blacklistHTML+"\n"
+            html = rawString.encode('utf-8')
+            conn.send(html)
+            conn.close()
+            break;
 
 
-def proxy_server(webserver, port, conn, addr, data, temp):
+def proxy_server(webserver, port, conn, addr, data, temp, threadID, startTime):
     fullReply = bytearray()
+    timeDifference = 0.0
     try:
-        print("[*] Request recieved from endpoint : " , temp)
+        print("[*] Request recieved from endpoint : " , webserver, " handled by thread : ", threadID)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
         urlResponse = checkCache(temp)
         if(urlResponse != None):
             conn.send(urlResponse)
@@ -100,8 +104,9 @@ def proxy_server(webserver, port, conn, addr, data, temp):
             dar = float(dar/1024)
             dar = "%.3s" % (str(dar))
             dar = "%s KB" % (dar)
-            print("[*] Request relayed to client : => %s <=" % (str(dar)))
-            print("----------- SENDING CACHED RESPONSE FINISHED -------------")
+            print("[*] Saved bandwidth : => %s <=" % (str(dar)))
+            print("[*] Request Processed in : => %s seconds <= " % (str(time.time()-startTime)))
+            print("----------- SENDING CACHED RESPONSE FINISHED -------------\n\n")
         else:
             s.connect((webserver, port))
             s.send(data)
@@ -110,22 +115,28 @@ def proxy_server(webserver, port, conn, addr, data, temp):
                 if(len(reply)>0):
                     conn.sendall(reply)
                     fullReply += reply
-                    dar = float(len(reply))
-                    dar = float(dar/1024)
-                    dar = "%.3s" % (str(dar))
-                    dar = "%s KB" % (dar)
-                    print("[*] Request relayed to client : => %s <=" % (str(dar)))
+                    timeDifference = (time.time()-startTime)
                 else:
                     break
-                print("[*] Cached " + temp + " : " + fullReply.decode())
-                addCache(temp, fullReply)
+
+    except (BlockingIOError, socket.timeout, OSError):
+        print("[*] Caching " + temp)
+        addCache(temp, fullReply)
+        dar = float(len(fullReply))
+        dar = float(dar/1024)
+        dar = "%.3s" % (str(dar))
+        dar = "%s KB" % (dar)
+        print("[*] Request relayed to client : => %s <=" % (str(dar)))
+        print("[*] Request Processed in : => %s seconds <=" % timeDifference)
+        print("----------- SENDING FETCHED RESPONSE FINISHED -------------\n\n")
+        pass
     except socket.error:
+        print("Error occured in thread")
         s.close()
         conn.close()
         sys.exit(1)
     s.close()
     conn.close()
-    time.sleep(20)
 
 def checkCache(url):
     try :
@@ -137,14 +148,61 @@ def checkCache(url):
 
 
 def addCache(url, reply):
-    print("[*] Adding URL to cache : ",url)
     cache.__setitem__(url, reply)
 
-def handleManagerReq():
+def handleManagerReq(req, threadID):
 
+    print("[*] MANAGER CONNECTION HANDLED BY : ",threadID)
+    payload = req[len(req)-1]   # extracts string containing json payload
+    json_acceptable_payload = payload.replace("'", "\"") # creates json payload
+    json_payload = json.loads(json_acceptable_payload)
+    print('json : ', json_payload)
+    if json_payload["func"] == "blklst":
+        blacklist.append(json_payload["url"])
+        print('[*] blacklist updated, ADDED : ', json_payload["url"])
+    if json_payload["func"] == "rmblk":
+        blacklist.remove(json_payload["url"])
+        print('[*] blacklist updated, REMOVED : ', json_payload["url"])
+    if json_payload["func"] == "usrBan":
+        blacklist.remove(json_payload["url"])
+        print('[*] blacklist updated, REMOVED : ', json_payload["url"])
 
-    httpResponse = []
-    for entry in cache:
-        httpResponse.append(cache[entry])
-    return httpResponse
+blacklistHTML = """
+    <!doctype html>
+    <html> <head> <title>BLACKLISTED Domain</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style type="text/css">
+        body {
+            background-color: #f0f0f2;
+            margin: 0;
+            padding: 0;
+            font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;}
+        div {
+            width: 600px;
+            margin: 5em auto;
+            padding: 2em;
+            background-color: #fdfdff;
+            border-radius: 0.5em;
+            box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
+        }
+        a:link, a:visited {
+            color: #38488f;
+            text-decoration: none;
+        }
+        @media (max-width: 700px) {
+            div {
+                margin: 0 auto;
+                width: auto;
+            }
+        }
+        </style>
+    </head>
+    <body>
+    <div>
+        <h1>BLACKLISTED Domain</h1>
+        <p>This domain is BLACKLISTED. You need to contact the proxy manager to enquire about access to this endpoint</p>
+        <p><a ">More information...</a></p>"""
+
 start()
